11de5524863026179c894e8fd24ae6c44ef0ac96
Badma Ontaev <ontaev@gmail.com>
2020-01-23 17:56:55 +0300

    added TodoDataset

---
+++ TodoDataset.py
@@ -0,0 +1,41 @@
+from torch.utils.data import Dataset
+
+class TodoDataset(Dataset):
+    def __init__(self, file_path):
+        """ init dataset """
+        self.samples = []
+        self.file_path = file_path
+        self._init_dataset()
+
+    def __len__(self):
+        """ returns size of dataset """
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        """ returns element by index """
+        return self.samples[idx]
+    
+    def _init_dataset(self):
+
+        with open(self.file_path + "/words.txt", 'r') as input_file:
+            for line in input_file:
+                line_split = line.strip().split('\t')
+                file_name = self.file_path+"/words/"+line_split[1]
+                gt_text = line_split[0]
+                self.samples.append((gt_text, file_name))
+        input_file.close()
+
+
+
+
+if __name__ == '__main__':
+
+    from torch.utils.data import DataLoader
+
+    dataset = TodoDataset('DATASET')
+    dataloader = DataLoader(dataset, batch_size=50, shuffle=True, num_workers=2)
+    for i, batch in enumerate(dataloader):
+        print(i, batch)
+    #print(len(dataset))
+    #print(dataset[100])
+    #print(dataset[122:361])
