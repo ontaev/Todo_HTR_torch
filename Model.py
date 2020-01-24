c59bb427aa3452c9ba84955cfc2c0420f093c83a
Badma Ontaev <ontaev@gmail.com>
2020-01-24 15:32:43 +0300

    added Model template

---
+++ Model.py
@@ -0,0 +1,20 @@
+import torch
+
+class Model:
+    """ NN Model for Todo Bicig recognition"""
+
+    def __init__(self):
+
+        self.setup_CNN()
+        self.setup_RNN()
+        self.setup_CTC()
+
+    def setup_CNN(self):
+        pass
+
+    def setup_RNN(self):
+        pass
+
+    def setup_CTC(self):
+        pass
+
