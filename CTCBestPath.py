369533dcc6b4b5812916248c9a294dccac23cbfb
Badma Ontaev <ontaev@gmail.com>
2020-01-30 09:17:57 +0300

    added copyright

--- CTCBestPath.py
+++ CTCBestPath.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------- #
+#            This code was taken from github repo:                  #
+# "Connectionist Temporal Classification (CTC) decoding algorithms" #
+#                developed by Harald Scheidl                        #
+#         https://github.com/githubharald/CTCDecoder/               #
+#               Copyright (c) 2018 Harald Scheidl                   #
+# ----------------------------------------------------------------- #
+
 from __future__ import division
 from __future__ import print_function
 from itertools import groupby
@@ -9,9 +17,9 @@ def ctcBestPath(mat, classes):
 
     # get char indices along best path
     best_path = np.argmax(mat, axis=1)
-    #print('bp', best_path)
+    
     # collapse best path (using itertools.groupby), map to chars, join char list to string
-    blank_idx = 0
+    blank_idx = 0 # index of CTC BLANK character (in original repo: blank_idx = len(classes))
     best_chars_collapsed = [classes[k] for k, _ in groupby(best_path) if k != blank_idx]
     res = ''.join(best_chars_collapsed)
     return res
